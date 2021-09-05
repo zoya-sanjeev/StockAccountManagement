678f8b010275e800324fdc82bbf4c43889e5870a
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-05 17:23:06 +0530

    ADDED: getters and setters added for class variables

--- src/com/bridgelabz/stockmanagement/Stock.java
+++ src/com/bridgelabz/stockmanagement/Stock.java
@@ -11,5 +11,30 @@ public class Stock {
 		this.sharePrice=sharePrice;
 	}
 	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getNumberOfShares() {
+		return numberOfShares;
+	}
+
+	public void setNumberOfShares(int numberOfShares) {
+		this.numberOfShares = numberOfShares;
+	}
+
+	public double getSharePrice() {
+		return sharePrice;
+	}
+
+	public void setSharePrice(double sharePrice) {
+		this.sharePrice = sharePrice;
+	}
+
+	
 
 }
