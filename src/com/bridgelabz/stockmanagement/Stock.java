476a0ecb86cbea14d3f92419695c56cda3adc6bc
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-05 17:16:54 +0530

    ADDED: constructor added for Stock'

--- src/com/bridgelabz/stockmanagement/Stock.java
+++ src/com/bridgelabz/stockmanagement/Stock.java
@@ -2,8 +2,14 @@ package com.bridgelabz.stockmanagement;
 
 public class Stock {
 	private String name;
-	private int NumberOfShares;
+	private int numberOfShares;
 	private double sharePrice;
+	
+	public Stock(String name,int numberOfShares, double sharePrice) {
+		this.name=name;
+		this.numberOfShares=numberOfShares;
+		this.sharePrice=sharePrice;
+	}
 	
 
 }
