a2a7b7250d8c6748a3b08f48a9863dc1aa2af3b5
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-09-05 17:36:22 +0530

    ADDED: constructor added for StockPortfolio

--- src/com/bridgelabz/stockmanagement/StockPortfolio.java
+++ src/com/bridgelabz/stockmanagement/StockPortfolio.java
@@ -1,5 +1,12 @@
 package com.bridgelabz.stockmanagement;
 
+import java.util.*;
+
 public class StockPortfolio {
 
+	List<Stock> listOfStocks;
+	
+	public StockPortfolio() {
+		this.listOfStocks=new ArrayList<>();
+	}
 }
